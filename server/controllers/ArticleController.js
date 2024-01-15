9593d43747fc811e0e84cd443be7536f3d28f271
ssselderey <79336152+ssselderey@users.noreply.github.com>
2024-01-15 15:22:08 +0300

    Create ArticleController.js

---
+++ server/controllers/ArticleController.js
@@ -0,0 +1,13 @@
+class ArticleController{ // класс для статьи изменение и получение
+    async creat(req, res){
+
+    }
+    async getALL(req, res){
+
+    }
+    async getOne(req, res){
+
+    }
+}
+
+module.exports = new ArticleController()
